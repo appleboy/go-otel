e4601a2e0a7568792c45869ea8586c118931ba38
Bo-Yi.Wu <appleboy.tw@gmail.com>
2022-09-24 15:58:42 +0800

    refactor(signoz): support custom setting

--- signoz/signoz.go
+++ signoz/signoz.go
@@ -3,6 +3,7 @@ package signoz
 import (
 	"context"
 	"io"
+	"strings"
 
 	core "github.com/appleboy/go-otel"
 
@@ -18,40 +19,88 @@ import (
 	"google.golang.org/grpc/credentials"
 )
 
-type Service struct {
-	name string
-	tp   *sdktrace.TracerProvider
+type Option interface {
+	apply(cfg *service)
 }
 
-func (s *Service) Tracer(opts ...trace.TracerOption) trace.Tracer {
+type option func(cfg *service)
+
+func (fn option) apply(cfg *service) {
+	fn(cfg)
+}
+
+// WithServiceVersion configures `service.version` resource attribute, for example, `1.0.0`.
+func WithVersion(serviceVersion string) Option {
+	return option(func(s *service) {
+		s.version = serviceVersion
+	})
+}
+
+// WithEnvironment configures `service.environment` resource attribute,
+// for example, `production`.
+func WithEnvironment(env string) Option {
+	return option(func(s *service) {
+		s.environment = env
+	})
+}
+
+func WithHeaders(envs map[string]string) Option {
+	return option(func(s *service) {
+		s.headers = envs
+	})
+}
+
+type service struct {
+	name         string
+	version      string
+	environment  string
+	collectorURL string
+	headers      map[string]string
+	tp           *sdktrace.TracerProvider
+}
+
+func (s *service) Tracer(opts ...trace.TracerOption) trace.Tracer {
 	return otel.Tracer(s.name, opts...)
 }
 
-func (s *Service) Shutdown(ctx context.Context) error {
+func (s *service) Shutdown(ctx context.Context) error {
 	return s.tp.Shutdown(ctx)
 }
 
 func New(
-	serviceName string,
-	collectorURL string,
-	insecure bool,
+	name string,
+	opts ...Option,
 ) (core.TracerProvider, error) {
+	s := &service{
+		name: name,
+	}
+
+	for _, opt := range opts {
+		opt.apply(s)
+	}
+
 	var exporter sdktrace.SpanExporter
 	var err error
 
-	if collectorURL != "" {
+	if s.collectorURL != "" {
 		headers := map[string]string{}
 
 		secureOption := otlptracegrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, ""))
-		if insecure {
+		if s.insecure() {
 			secureOption = otlptracegrpc.WithInsecure()
+		}
+
+		if s.headers != nil {
+			for k, v := range s.headers {
+				headers[k] = v
+			}
 		}
 
 		exporter, err = otlptrace.New(
 			context.Background(),
 			otlptracegrpc.NewClient(
 				secureOption,
-				otlptracegrpc.WithEndpoint(collectorURL),
+				otlptracegrpc.WithEndpoint(s.collectorURL),
 				otlptracegrpc.WithHeaders(headers),
 			),
 		)
@@ -71,8 +120,10 @@ func New(
 	resources, err := resource.New(
 		context.Background(),
 		resource.WithAttributes(
-			attribute.String("service.name", serviceName),
-			attribute.String("library.language", "go"),
+			attribute.String("service.name", s.name),
+			attribute.String("service.language", "go"),
+			attribute.String("service.version", s.version),
+			attribute.String("service.environment", s.environment),
 		),
 	)
 	if err != nil {
@@ -81,18 +132,19 @@ func New(
 
 	// For the demonstration, use sdktrace.AlwaysSample sampler to sample all traces.
 	// In a production application, use sdktrace.ProbabilitySampler with a desired probability.
-	traceProvider := sdktrace.NewTracerProvider(
+	s.tp = sdktrace.NewTracerProvider(
 		sdktrace.WithSampler(sdktrace.AlwaysSample()),
 		sdktrace.WithSpanProcessor(sdktrace.NewBatchSpanProcessor(exporter)),
 		sdktrace.WithSyncer(exporter),
 		sdktrace.WithBatcher(exporter),
 		sdktrace.WithResource(resources),
 	)
-	otel.SetTracerProvider(traceProvider)
+	otel.SetTracerProvider(s.tp)
 	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
 
-	return &Service{
-		tp:   traceProvider,
-		name: serviceName,
-	}, nil
+	return s, nil
+}
+
+func (s *service) insecure() bool {
+	return !strings.HasPrefix(s.collectorURL, "https://")
 }
